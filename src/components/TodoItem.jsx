ddc49f54107e448243fc603f0b5674d3e3d1b18e
doan dung <doanminhdung662@gmail.com>
2025-03-19 15:05:20 +0700

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+
+const TodoItem = (props) => {
+  return (
+    <div
+      className="todo-item"
+      onClick={() => {
+        props.handleTodoItemClick(props.id);
+      }}
+    >
+      <div style={{ display: "flex", gap: 4 }}>
+        <input
+          type="checkbox"
+          checked={props.isCompleted}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          onChange={() => {
+            props.handleCompleteCheckboxChange(props.id);
+          }}
+        />
+        <p className="todo-item-text">{props.name}</p>
+      </div>
+      {props.isImportant && <p>⭐</p>}
+    </div>
+  );
+};
+
+TodoItem.propTypes = {
+  name: PropTypes.string,
+  id: PropTypes.string,
+  isImportant: PropTypes.bool,
+  isCompleted: PropTypes.bool,
+  handleTodoItemClick: PropTypes.func,
+  handleCompleteCheckboxChange: PropTypes.func,
+};
+
+export default TodoItem;
